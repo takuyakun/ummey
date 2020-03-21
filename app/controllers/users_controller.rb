a28cd5e7fd2311d531f9e37195201128d7671bb6
takuyakun <dorachiki0227@icloud.com>
2020-03-21 12:52:37 +0900

    Add microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -49,14 +49,6 @@ private
  def user_params
   params.require(:user).permit(:name, :email, :password,
                                :password_confirmation)
- end
-
- def logged_in_user
-  unless logged_in?
-    store_location
-    flash[:danger] = "Please log in."
-    redirect_to login_url
-  end
  end
 
  def correct_user
