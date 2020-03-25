846a32aafb11b5da124a7ffd22c9f3968e9214af
takuyakun <dorachiki0227@icloud.com>
2020-03-25 15:52:18 +0900

    Add links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@
 
 class HomeController < ApplicationController
   def top
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 end
