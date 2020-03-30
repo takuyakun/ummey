3d9b02d42c86dc6d014781187fd4e5fb57024bba
takuyakun <dorachiki0227@icloud.com>
2020-03-30 15:09:35 +0900

    Change layout

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,13 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user, only: [:index, :create, :destroy]
   before_action :correct_user,   only: :destroy
+
+  def index
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.page(params[:page])
+    end
+  end
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
